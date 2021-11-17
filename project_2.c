6e10698537fbda788f90834a5f32e3bbc03e6a3c
Naphat Nithisopa <pection.naphat@gmail.com>
2021-11-17 21:00:01 +0700

    update project file

--- project_2.c
+++ project_2.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-int max_rows=100;
-void createHead(){
-  printf("|");
-  for(int i =0; i<max_rows;i++){
-    printf("_");
-  }
-  printf("|\n");
-}
-void addMenu(){
-  char Menu[4]="MENU";
-  printf("|");
-  for(int i =0; i<max_rows-strlen(Menu);i++){
-    if(i == 44){
-      printf("%s",Menu);
-    }
-    printf("_");
-  }
-  printf("|\n");
-}
-
+#include "functions.c"
 void main(){
   createHead();
   addMenu();
